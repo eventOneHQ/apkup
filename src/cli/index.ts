550ed03d649a0da2982873e1e0b09164e2f2f39c
Noah Prail <nprail@event1.io>
2019-10-05 08:08:58 -0400

    chore: Initial work on adding config file

--- src/cli/index.ts
+++ src/cli/index.ts
@@ -6,9 +6,10 @@ import yargs from 'yargs'
 import { promote } from './promote'
 import { upload } from './upload'
 
-const pkg = JSON.parse(
-  fs.readFileSync(path.join(__dirname, '../../package.json'), 'utf-8')
-)
+const readJson = (filePath: string) =>
+  JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+
+const pkg = readJson(path.join(__dirname, '../../package.json'))
 
 const argv = yargs
   .usage('Usage: $0 [options]')
@@ -23,11 +24,32 @@ const argv = yargs
     describe: 'Path to the APK file',
     type: 'array'
   })
+  .option('packageName', {
+    alias: 'p',
+    describe: 'Name of the package (e.g. com.example.yourapp)',
+    type: 'string',
+    demandOption: true
+  })
+  .config('config', 'Path to a JSON config file', (configPath: string) => {
+    const config = readJson(configPath)
+    if (config.key) {
+      config.auth = readJson(config.key)
+    }
+    return {
+      packageName: config.packageName,
+      p: config.packageName,
+      apk: config.apk,
+      a: config.apk,
+      key: config.key,
+      k: config.k,
+      auth: config.auth
+    }
+  })
   .config(
     'key',
     'Path to a JSON file that contains the private key and client email (can be specified via APKUP_KEY env variable)',
-    (configPath) => {
-      return { auth: JSON.parse(fs.readFileSync(configPath, 'utf-8')) }
+    (configPath: string) => {
+      return { auth: readJson(configPath) }
     }
   )
   .command(promote)
